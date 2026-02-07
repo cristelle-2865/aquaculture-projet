06c7f7d6e2f3d685a06985d21bdde78edf2c7573
Nickah-crea <andrianarisoa3544@gmail.com>
2026-02-07 15:33:28 +0100

    all

---
+++ aquaculture-backend/src/main/java/com/example/aquaculture/AquacultureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aquaculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquacultureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquacultureBackendApplication.class, args);
+	}
+
+}
